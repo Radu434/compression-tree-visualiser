84c3aeae6223be6078f905cd098ebc7df55c6d5d
Pestesan Radu <radupestesan@yahoo.com>
2025-04-29 23:12:56 +0300

    Sorting by frequency

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
+import { useMemo } from "react";
 import "./index.css";
 import TextInput from "./components/TextInput";
 import HuffmanTreeVisualizer from "./components/HuffmanTreeVisualiser";
 import HuffmanService from "./services/HuffmanService.js";
 import nodeToD3Format from "./services/NodeToD3Format.js";
+import { sort } from "d3";
 
 function App() {
   const [text, setText] = useState(""); // State for the input text
@@ -13,6 +15,9 @@ function App() {
   const [compressedBytes, setCompressedBytes] = useState(null);
   const [uncompressedBytes, setUnCompressedBytes] = useState(null);
   const [frequencyMap, setFrequencyMap] = useState(null);
+
+
+  
   const handleHuffmanCompress = (inputText) => {
     if (!inputText) {
       return null;
@@ -56,68 +61,62 @@ function App() {
       setCompressedBytes(compressedBytes);
     }
   };
-
+  const sortedCodes = useMemo(() => {
+    if (!huffmanCodes || !frequencyMap) return [];
+  
+    return [...huffmanCodes.entries()].sort(([charA], [charB]) => {
+      const freqA = frequencyMap.get(charA);
+      const freqB = frequencyMap.get(charB);
+      return freqA === freqB
+        ? charA.localeCompare(charB)
+        : freqB - freqA;
+    });
+  }, [huffmanCodes, frequencyMap]);
   return (
-    <div className="flex flex-col items-center justify-start min-h-screen bg-gray-900">
-      <div className="flex justify-center w-full bg-blue-900 shadow-black shadow-lg/1.2 sticky top-0 z-1">
-        <h1 className="text-white px-1 hover:text-amber-200 rounded-2xl">
-          Huffman Compression Visualiser
-        </h1>
-      </div>
-      {/* Pass the text and handler to TextInput */}
-      <div
-        className={"flex flex-row items-center justify-center w-full h-full"}
-      >
-        <TextInput
-          text={text}
-          setText={setText}
-          onHuffmanCompress={handleHuffmanCompress}
-          setIterative={setIterative}
-        />
-        {huffmanCodes && (
-          <div className="flex flex-col items-center justify-center max-h-80 border-blue-800 border-2 rounded-lg overflow-hidden ">
-            <div className="relative overflow-x-auto shadow-md sm:rounded-lg ">
-              <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 sticky top-0">
-                  <tr>
-                    <th scope="col" className="px-6 py-3">
-                      Character
-                    </th>
-                    <th scope="col" className="px-12.5 py-3">
-                      Code
-                    </th>
-                    <th scope="col" className="px-12.5 py-3">
-                      Frequency
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {[...huffmanCodes.entries()]
-                    .sort(([, codeA], [, codeB]) => codeA.length - codeB.length)
-                    .map(([char, code], index) => (
-                      <tr
-                        key={index}
-                        className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200"
-                      >
-                        <th
-                          scope="row"
-                          className="px-12 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                        >
-                          {char === " "
-                            ? "[space]"
-                            : char === "\n"
-                            ? "[\\n]"
-                            : char}
-                        </th>
-                        <td className="px-14 py-4">{code}</td>
-                        <td className="px-20 py-4">{frequencyMap.get(char)}</td>
-
-                      </tr>
-                    ))}
-                </tbody>
-              </table>
-            </div>
-          </div>
+<div className="flex flex-col items-center justify-start min-h-screen bg-gray-900 px-4 sm:px-10">
+<div className="flex justify-center w-screen bg-blue-900 shadow-black shadow-lg sticky top-0 z-10">
+  <h1 className="text-white px-1 hover:text-amber-200 text-lg sm:text-2xl">
+    Huffman Compression Visualiser
+  </h1>
+</div>
+<div className="flex flex-col sm:flex-row items-center justify-center w-full gap-4 ">
+    <TextInput
+      text={text}
+      setText={setText}
+      onHuffmanCompress={handleHuffmanCompress}
+      setIterative={setIterative}
+    />  
+        {huffmanCodes &&frequencyMap && (
+     <div className="relative max-h-80   overflow-x-auto shadow-md sm:rounded-lg">
+     <table className="w-full  text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+       <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 sticky top-0">
+         <tr>
+           <th scope="col" className="px-6 py-3">Character</th>
+           <th scope="col" className="px-6 py-3">Code</th>
+           <th scope="col" className="px-6 py-3">Frequency</th>
+         </tr>
+       </thead>
+       <tbody>
+         {sortedCodes
+    
+           .map(([char, code], index) => (
+             <tr
+               key={index}
+               className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200"
+             >
+               <th
+                 scope="row"
+                 className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+               >
+                 {char === " " ? "[space]" : char === "\n" ? "[\\n]" : char}
+               </th>
+               <td className="px-6 py-4">{code}</td>
+               <td className="px-6 py-4">{frequencyMap.get(char)}</td>
+             </tr>
+           ))}
+       </tbody>
+     </table>
+   </div>
         )}
       </div>
       {huffmanTree && (
@@ -139,9 +138,9 @@ function App() {
         </div>
       )}
       <div
-        className="flex flex-col items-center
-             justify-center max-w-9/10 w-400 h-150 border-2 border-blue-500 bg-white
-              mb-10 overflow-hidden rounded-lg"
+        className="flex flex-col items-center justify-center
+             w-full h-[300px] sm:h-[500px] bg-white border-2 border-blue-500
+             mb-10 overflow-hidden rounded-lg"
       >
         {huffmanTree && <HuffmanTreeVisualizer treeData={huffmanTree} />}
       </div>
--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,68 +1,69 @@
 import React from "react";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faGear} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGear } from "@fortawesome/free-solid-svg-icons";
 
-function TextInput({text, setText, onHuffmanCompress, setIterative}) {
-    const handleInputChange = (e) => {
-        setText(e.target.value); // Update the text state in App.jsx
-    };
-    const handleCheckChange = (e) => {
-        setIterative(e.target.checked); // Update checked state in App.jsx
-    }
+function TextInput({ text, setText, onHuffmanCompress, setIterative }) {
+  const handleInputChange = (e) => {
+    setText(e.target.value); // Update the text state in App.jsx
+  };
+  const handleCheckChange = (e) => {
+    setIterative(e.target.checked); // Update checked state in App.jsx
+  };
 
-    const handleButtonClick = () => {
-        onHuffmanCompress(text); // Trigger the Huffman compression logic
-    };
+  const handleButtonClick = () => {
+    onHuffmanCompress(text); // Trigger the Huffman compression logic
+  };
 
-
-    return (
-        <div className="mt-10 p-10">
-            <form className="flex flex-col mx-auto gap-2 max-w-[vw-2/3]">
-                <fieldset className="contents">
-                    <div className="flex flex-col">
+  return (
+    <div className="mt-10 p-10">
+      <form className="flex flex-col mx-auto gap-2 max-w-[vw-2/3]">
+        <fieldset className="contents">
+          <div className="flex flex-col">
             <textarea
-                name="input"
-                id="input"
-                rows="10"
-                maxLength="260416"
-                placeholder="Insert the text you want to compress. [Max 260416 chars]"
-                value={text} // Controlled input
-                onChange={handleInputChange}
-                className="rounded-lg p-4 bg-gray-800 border-2 border-solid
-               border-gray-550 font-mono font-medium text-sm text-white min-w-[600px]
-                focus:outline-none focus:ring-2 focus:bg-blue-950"></textarea>
-                    </div>
-                    <div className="flex flex-row items-center justify-center mt-4">
-                        <button
-                            type="button"
-                            onClick={handleButtonClick} // Call the handler
-                            className="rounded-lg p-2 bg-blue-950 border-2 border-solid
-               border-blue-500 transition-colors
-                hover:bg-blue-500
-                text-white font-medium text-base leading-none
-                 font-small 
-                 flex flex-row items-center
-                 justify-center gap-2">
-                            <FontAwesomeIcon icon={faGear}/>
-                            <span className="font-semibold">Compress Using Huffman</span>
-                        </button>
-                        <input
-                            onChange={handleCheckChange}
-                            type="checkbox"
-                            id="iterative"
-                            className="ml-4 mr-0 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded
-                             focus:ring-blue-500 focus:ring-2, hover:bg-blue-600"
-                        ></input>
-                        <label
-                            htmlFor="iterative"
-                            className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300
-                            hover:text-blue-800"
-                        >Step-by-Step</label>
-                    </div>
-                </fieldset>
-            </form>
-        </div>
-    );
+              name="input"
+              id="input"
+              rows="10"
+              maxLength="260416"
+              placeholder="Insert the text you want to compress. [Max 260416 chars]"
+              value={text} // Controlled input
+              onChange={handleInputChange}
+              className="rounded-lg p-4 bg-gray-800 border-2 border-solid
+             border-gray-550 font-mono font-medium text-sm text-white
+             w-full sm:w-[600px] focus:outline-none focus:ring-2 focus:bg-blue-950"
+            ></textarea>
+          </div>
+          <div className="flex flex-col sm:flex-row items-center gap-4 justify-center mt-4">
+            <button
+              type="button"
+              onClick={handleButtonClick}
+              className="rounded-lg p-2 bg-blue-950 border-2 border-solid
+               border-blue-500 transition-colors hover:bg-blue-500
+               text-white font-medium text-base leading-none flex
+               items-center justify-center gap-2 w-full sm:w-auto"
+            >
+              <FontAwesomeIcon icon={faGear} />
+              <span className="font-semibold">Compress Using Huffman</span>
+            </button>
+            <div className="flex items-center gap-2">
+              <input
+                onChange={handleCheckChange}
+                type="checkbox"
+                id="iterative"
+                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded
+                 focus:ring-blue-500 focus:ring-2"
+              />
+              <label
+                htmlFor="iterative"
+                className="text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Step-by-Step
+              </label>
+            </div>
+          </div>
+        </fieldset>
+      </form>
+    </div>
+  );
 }
 
 export default TextInput;
